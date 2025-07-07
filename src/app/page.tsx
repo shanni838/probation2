a80e468434375725dfadd8580ed8c02a06dd72e6
prisella-work <prisella.work.mail@gmail.com>
2025-07-07 07:23:57 +0000

    add name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-    <h1>hello shanni</h1>
+    <h1>hello shanni, ini naomi</h1>
     <select name="form" >
       <option value="1">250</option>
       <option value="2">500</option>
