f6de7ba19cf976707d84395b0e3a2ba423780292
shanni838 <anselialie21@gmail.com>
2025-07-07 14:03:23 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <h1>hello shanni</h1>
+    <select name="form" >
+      <option value="1">250</option>
+      <option value="2">500</option>
+      <option value="3">1000</option>
+      <option value="4">2000</option>
+    </select>
+   
+    </div>
+  );
+}
