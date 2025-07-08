05a6e7b2256cbacc07941419bab8b3cc00fdeebf
shanni838 <anselialie21@gmail.com>
2025-07-08 10:52:26 +0700

    image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { getPosts } from '@/lib/posts';
+import { Post } from '@/ui/post';
 
 export default function Home() {
   return (
@@ -10,7 +12,13 @@ export default function Home() {
       <option value="3">1000</option>
       <option value="4">2000</option>
     </select>
-   
+    <input type="number" value="1" min="1"></input>
+    <Image src="/profil.png" alt="Profile" width={500} height={1500} />
+    <ul>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </ul>
     </div>
-  );
+  )
 }
